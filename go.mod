8a5a247c5c395f820549d75f56143b86b0814132
chad-bekmezian-snap <chad.bekmezian@snapone.com>
2026-04-07 10:03:07 -0600

    feat: support sprig funcs

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/rad12000/go-sfgen
 go 1.24
 
 require (
+	github.com/Masterminds/sprig/v3 v3.3.0
 	github.com/fatih/structtag v1.2.0
 	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510
 	github.com/stretchr/testify v1.11.1
@@ -10,8 +11,18 @@ require (
 )
 
 require (
+	dario.cat/mergo v1.0.1 // indirect
+	github.com/Masterminds/goutils v1.1.1 // indirect
+	github.com/Masterminds/semver/v3 v3.3.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/huandu/xstrings v1.5.0 // indirect
+	github.com/mitchellh/copystructure v1.2.0 // indirect
+	github.com/mitchellh/reflectwalk v1.0.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/shopspring/decimal v1.4.0 // indirect
+	github.com/spf13/cast v1.7.0 // indirect
+	golang.org/x/crypto v0.26.0 // indirect
 	golang.org/x/mod v0.22.0 // indirect
 	golang.org/x/sync v0.10.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- template/template.go
+++ template/template.go
@@ -1,3 +1,20 @@
+// Package template provides types and functions for executing to templates.
+// All templates are provided the [Data] type as its argument.
+//
+// Each template also has access to the following functions:
+//
+//   - [Builtin Template Functions]
+//
+//   - [Sprig Functions]: the `slice` sprig function is renamed to `slice_list` to avoid conflicts with the built-in `slice` function, which has different behavior.
+//
+//   - func fatalf(format string, args ..any): exits the program with the formatted error message.
+//
+//   - func to_lower(s string) string: returns the lowercase version of the input string.
+//
+//   - func require_imports(imps []string): adds the provided imports to the list of required imports for the generated code.
+//
+// [Sprig Functions]: https://masterminds.github.io/sprig/string_slice.html
+// [Builtin Template Functions]: https://pkg.go.dev/text/template#hdr-Functions
 package template
 
 import (
@@ -7,6 +24,8 @@ import (
 	"strings"
 	"sync"
 	"text/template"
+
+	"github.com/Masterminds/sprig/v3"
 )
 
 type Template struct {
@@ -40,19 +59,22 @@ func New(loadTemplate func(*template.Template) (*template.Template, error)) *Tem
 		imports: imports,
 		loadTemplate: sync.OnceValues(func() (*template.Template, error) {
 			tmpl := template.New("root")
-			tmpl.Funcs(template.FuncMap{
-				"fatalf": func(format string, args ...any) string {
-					log.Fatalf(format, args...)
-					return ""
-				},
-				"to_lower": strings.ToLower,
-				"require_imports": func(imps []string) string {
-					for _, imp := range imps {
-						imports[imp] = struct{}{}
-					}
-					return ""
-				},
-			})
+			sprigFuncs := sprig.HermeticTxtFuncMap()
+			sprigFuncs["slice_list"] = sprigFuncs["slice"]
+			delete(sprigFuncs, "slice")
+
+			sprigFuncs["fatalf"] = func(format string, args ...any) string {
+				log.Fatalf(format, args...)
+				return ""
+			}
+			sprigFuncs["to_lower"] = strings.ToLower
+			sprigFuncs["require_imports"] = func(imps []string) string {
+				for _, imp := range imps {
+					imports[imp] = struct{}{}
+				}
+				return ""
+			}
+			tmpl.Funcs(sprigFuncs)
 
 			tmpl, err := loadTemplate(tmpl)
 			if err != nil {
